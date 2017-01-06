a8baa07e7d6ee7edd2f20d5c49e08ee430e9bf5a
hymRedemption <he.redemption@gmail.com>
2017-01-06 14:34:25 +0800

    Implement the generate_contract logic

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,20 +1,35 @@
 class Contract < ApplicationRecord
+  has_many :renting_phases
+
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :date_comfirm
 
-  has_many :renting_phases
 
   def self.generate_contract(options)
     start_date = options[:start_date]
     end_date = options[:end_date]
-    due_date = options[:due_date]
-    phase_num = options.fetch(:phase_num, 1)
-    contract = self.create(start_date: start_date, end_date: end_date)
-    phase_num.times do
-      contract.renting_phases.create(start_date: start_date, end_date: end_date)
+    renting_phases_param = options[:renting_phases]
+    contract = self.create!(start_date: start_date, end_date: end_date)
+    phase_start_date = start_date
+    renting_phases_param.each do |renting_phase|
+      phase_end_date = renting_phase[:end_date]
+      raise TimeRangeError if phase_start_date > phase_end_date
+      renting_phase[:start_date] = phase_start_date
+      phase_start_date = renting_phase[:end_date].days_since(1)
     end
+    contract.renting_phases.create!(renting_phases_param)
 
     return contract
   end
 
+  private
+
+  def date_comfirm
+    if start_date >  end_date
+      errors.add(:date_invalid, "start_date can't be later than end_date")
+    end
+  end
+
+  class TimeRangeError < ArgumentError; end
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,25 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Contract, type: :model do
+  let(:start_date) { Date.new(2000, 1, 31) }
+  let(:end_date) { Date.new(2000, 10, 8) }
+  it 'should invalid if end_date smaller than start_date' do
+    contract = FactoryGirl.build(:contract, start_date: start_date, end_date: start_date.days_ago(2))
+    expect(contract.valid?).to eq(false)
+  end
+
   context '.generate_contract' do
-    let(:start_date) { Date.new(2000, 1, 10) }
-    let(:end_date) { Date.new(2000, 10, 8) }
+    let(:params) do
+      {
+        start_date: start_date,
+        end_date: end_date,
+        renting_phases: [
+          {
+            end_date: Date.new(2000, 2, 29),
+            price: 100,
+            cycles: 1
+          },
+          {
+            end_date: Date.new(2000, 6, 10),
+            price: 200,
+            cycles: 3
+          },
+          {
+            end_date: end_date,
+            price: 300,
+            cycles: 2
+          }
+        ]
+      }
+    end
+
     it 'generates contract' do
-      contract = Contract.generate_contract(start_date: start_date, end_date: end_date, price: 100)
+      contract = Contract.generate_contract(params)
       expect(contract.is_a?(Contract)).to eq(true)
     end
 
-    it 'has same renting phases as we set' do
-      phase_num = 2
-      contract = Contract.generate_contract(start_date: start_date, end_date: end_date, price: 100, phase_num: phase_num)
-      expect(contract.renting_phases.count).to eq(2)
+    context 'Renting Phase' do
+      context 'valid operation' do
+        let(:contract) { Contract.generate_contract(params) }
+        it 'has same renting phases as we set' do
+          expect(contract.renting_phases.count).to eq(3)
+        end
+
+        it 'has same price as we set of specific phase' do
+          last_renting_phase = contract.renting_phases.order(:end_date).last
+          expect(last_renting_phase.price).to eq(params[:renting_phases].last[:price])
+        end
+
+        it 'has same cicles as we set of specific phase' do
+          last_renting_phase = contract.renting_phases.order(:end_date).last
+          expect(last_renting_phase.cycles).to eq(params[:renting_phases].last[:cycles])
+        end
+
+        it 'has correct start_date' do
+          last_renting_phase = contract.renting_phases.order(:end_date).last
+          expect(last_renting_phase.start_date).to eq(Date.new(2000, 6, 10).days_since(1))
+        end
+      end
+
+      context 'invalid operation' do
+        it 'raise ArgumentError if end_date is earlier or equal than previous phase end_date' do
+          first_phase_end_date = params[:renting_phases].first[:end_date]
+          params[:renting_phases][1][:end_date] = first_phase_end_date
+          expect { Contract.generate_contract(params) }.to raise_error(Contract::TimeRangeError)
+        end
+      end
     end
-
-    it 'has one renting phase if we do not set phase num' do
-      contract = Contract.generate_contract(start_date: start_date, end_date: end_date, price: 100)
-      expect(contract.renting_phases.count).to eq(1)
-    end
-
-
   end
 end
