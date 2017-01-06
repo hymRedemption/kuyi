4eaff4a9c844bedb782b3149ce00c1ccb3fd6efe
hymRedemption <he.redemption@gmail.com>
2017-01-06 11:38:05 +0800

    .generate_contract must return Constant instance

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,3 +1,8 @@
 class Contract < ApplicationRecord
   has_many :renting_phases
+
+  def self.generate_contract(options)
+    return self.new
+  end
+
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Contract, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '.generate_contract' do
+    let(:start_date) { Date.new(2000, 1, 10) }
+    let(:end_date) { Date.new(2000, 10, 8) }
+    it 'generates contract' do
+      contract = Contract.generate_contract(start_date: start_date, end_date: end_date, price: 100)
+      expect(contract.is_a?(Contract)).to eq(true)
+    end
+  end
 end
