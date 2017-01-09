26c1fbcd280f6bccf834694a4cf7326e03c52041
hymRedemption <he.redemption@gmail.com>
2017-01-09 23:42:32 +0800

    Add test for DatePhase

--- spec/models/concerns/date_phase_spec.rb
+++ spec/models/concerns/date_phase_spec.rb
@@ -1,153 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe DatePhase do
-  let(:klass) { Class.new }
-  let(:include_klass) { klass.class_exec{ include DatePhase } }
-  let(:instance) { include_klass.new }
-  context '#months_between' do
-    context 'in the same month' do
-      it 'returns right num if begin at month first date and end at month end day' do
-        smaller_date = Date.new(2000, 1, 1)
-        bigger_date = Date.new(2000, 3, 31)
-        num = instance.months_between(smaller_date, bigger_date)
-        expect(num).to eq(3)
-      end
-
-      it 'returns right num for special' do
-        smaller_date = Date.new(2000, 1, 31)
-        bigger_date = Date.new(2000, 2, 29)
-        num = instance.months_between(smaller_date, bigger_date)
-        expect(num).to eq(1)
-      end
-
-      it 'returns right num if when is a month' do
-        smaller_date = Date.new(2000, 1, 2)
-        bigger_date = Date.new(2000, 1, 14)
-        num = instance.months_between(smaller_date, bigger_date)
-        expect(num).to eq(0)
-      end
-
-      it 'returns right num if when is not in a month' do
-        smaller_date = Date.new(2000, 1, 2)
-        bigger_date = Date.new(2000, 3, 14)
-        num = instance.months_between(smaller_date, bigger_date)
-        expect(num).to eq(2)
-      end
-    end
-
-    context '#monthlong_end_date_since' do
-      it 'returns right end date if days in predict month is not enough' do
-        date = instance.monthlong_end_date_since(1, Date.new(2000, 1, 31))
-        expect(date).to eq(Date.new(2000, 2, 29))
-      end
-
-      it 'returns right end date if days in predict month is enough' do
-        date = instance.monthlong_end_date_since(1, Date.new(2000, 1, 29))
-        expect(date).to eq(Date.new(2000, 2, 28))
-      end
-    end
-
-    context 'in the different month' do
-      it 'returns correct num when in different year' do
-        smaller_date = Date.new(1999, 12, 30)
-        bigger_date = Date.new(2000, 2, 29)
-        num = instance.months_between(smaller_date, bigger_date)
-        expect(num).to eq(2)
-      end
-
-      it 'returns correct num when the date month is enough for a date' do
-        smaller_date = Date.new(2000, 1, 29)
-        bigger_date = Date.new(2000, 2, 29)
-        num = instance.months_between(smaller_date, bigger_date)
-        expect(num).to eq(1)
-      end
-
-      it 'returns corrent num when the date is not enough' do
-        smaller_date = Date.new(2000, 1, 30)
-        bigger_date = Date.new(2000, 2, 28)
-        num = instance.months_between(smaller_date, bigger_date)
-        expect(num).to eq(0)
-      end
-
-      it 'returns correct num when middle date is specifal' do
-        smaller_date = Date.new(2000, 1, 1)
-        bigger_date = Date.new(2000, 3, 31)
-        num = instance.months_between(smaller_date, bigger_date)
-        expect(num).to eq(3)
-      end
+  let(:instance) do
+    Class.class_exec do
+      attr_accessor :start_date, :end_date, :months_in_phase
+      include DatePhase
     end
   end
 
-  context '#monthlong?' do
-    context 'same month' do
-      it 'returns true if days between dates is a whole month' do
-        smaller_date = Date.new(2000, 1, 1)
-        bigger_date = Date.new(2000, 1, 31)
-        expect(instance.monthlong?(smaller_date, bigger_date)).to eq(true)
+  context '#time_ranges_of_phases' do
+    context 'months in phase is 1' do
+      before :each do
+        instance.start_date = Date.new(1999, 12, 31)
+        instance.end_date = Date.new(2000, 4, 2)
+        instance.months_in_phase = 1
+      end
+      let(:result_of_time_ranges) do
+        [
+          {
+            start_date: Date.new(1999, 12, 31),
+            end_date: Date.new(2000, 1, 30)
+          },
+          {
+            start_date: Date.new(2000, 1, 31),
+            end_date: Date.new(2000, 2, 29)
+          },
+          {
+            start_date: Date.new(2000, 3, 1),
+            end_date: Date.new(2000, 3, 30)
+          },
+          {
+            start_date: Date.new(2000, 3, 31),
+            end_date: Date.new(2000, 4, 2)
+          }
+        ]
       end
 
-      it 'returns false if days between is not enough' do
-        smaller_date = Date.new(2000, 1, 1)
-        bigger_date = Date.new(2000, 1, 3)
-        expect(instance.monthlong?(smaller_date, bigger_date)).to eq(false)
+      it 'returns time ranges with right size' do
+        time_ranges = instance.time_ranges_of_phases
+        expect(time_ranges.size).to eq(result_of_time_ranges.size)
+      end
+
+      it 'returns right time ranges' do
+        time_ranges = instance.time_ranges_of_phases
+        expect(time_ranges).to eq(result_of_time_ranges)
       end
     end
 
-    context 'different month' do
-      it 'returns true if days between is whole month' do
-        smaller_date = Date.new(1999, 12, 30)
-        bigger_date = Date.new(2000, 2, 29)
-        expect(instance.monthlong?(smaller_date, bigger_date)).to eq(true)
+    context 'months in phase is more than 1' do
+      before :each do
+        instance.start_date = Date.new(1999, 12, 31)
+        instance.end_date = Date.new(2000, 4, 2)
+        instance.months_in_phase = 2
+      end
+      let(:result_of_time_ranges) do
+        [
+          {
+            start_date: Date.new(1999, 12, 31),
+            end_date: Date.new(2000, 2, 29)
+          },
+          {
+            start_date: Date.new(2000, 3, 1),
+            end_date: Date.new(2000, 4, 2)
+          }
+        ]
       end
 
-      it 'returns false if days between is not enough' do
-        smaller_date = Date.new(1999, 12, 30)
-        bigger_date = Date.new(2000, 2, 28)
-        expect(instance.monthlong?(smaller_date, bigger_date)).to eq(false)
+      it 'returns time ranges with right size' do
+        time_ranges = instance.time_ranges_of_phases
+        expect(time_ranges.size).to eq(result_of_time_ranges.size)
       end
-    end
-  end
 
-  context '#scattered_days_between' do
-    it 'retuns 0 if the dates between is monthlong' do
-      smaller_date = Date.new(2000, 1, 31)
-      bigger_date = Date.new(2000, 2, 29)
-      num = instance.scattered_days_between(smaller_date, bigger_date)
-      expect(num).to eq(0)
-    end
-
-    it 'returns the scattered days num between dates that in a month' do
-      smaller_date = Date.new(2000, 1, 10)
-      bigger_date = Date.new(2000, 1, 29)
-      num = instance.scattered_days_between(smaller_date, bigger_date)
-      expect(num).to eq(20)
-    end
-
-    it 'returns days num between dates that not in a month' do
-      smaller_date = Date.new(2000, 1, 10)
-      bigger_date = Date.new(2000, 2, 29)
-      num = instance.scattered_days_between(smaller_date, bigger_date)
-      expect(num).to eq(20)
-    end
-  end
-
-  context '#same_month?' do
-    it 'returns true if it in the same month' do
-      date1 = Date.new(2000, 1, 20)
-      date2 = Date.new(2000, 1, 1)
-      expect(instance.same_month?(date1, date2)).to eq(true)
-    end
-
-    it 'returns false if dates in different year' do
-      date1 = Date.new(1999, 1, 1)
-      date2 = Date.new(2000, 1, 1)
-      expect(instance.same_month?(date1, date2)).to eq(false)
-    end
-
-    it 'returns false if dates in different month' do
-      date1 = Date.new(1999, 2, 1)
-      date2 = Date.new(1999, 1, 1)
-      expect(instance.same_month?(date1, date2)).to eq(false)
+      it 'returns right time ranges' do
+        time_ranges = instance.time_ranges_of_phases
+        expect(time_ranges).to eq(result_of_time_ranges)
+      end
     end
   end
 end
