d4c0835c147b45a6bf2137217d7d3305133843e4
hymRedemption <he.redemption@gmail.com>
2017-01-10 09:57:23 +0800

    Refactor invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -16,25 +16,26 @@ class Invoice < ApplicationRecord
 
   def generate_items
     time_ranges_of_phases.map do |time_range|
-      total = renting_phase.price
-      item_start_date = time_range[:start_date]
-      item_end_date = time_range[:end_date]
+      units, unit_price = item_price_info(time_range[:start_date], time_range[:end_date])
+      item_params = {
+        unit_price: unit_price,
+        units: units,
+        total: units * unit_price,
+        invoice: self
+      }.merge(time_range)
+      LineItem.create(item_params)
+    end
+  end
+
+  private
+
+    def item_price_info(item_start_date, item_end_date)
       units = 1
       unit_price = renting_phase.price
       if !item_start_date.integral_months_to?(item_end_date)
         units = item_start_date.scattered_days_to(item_end_date)
         unit_price = renting_phase.price_per_day
       end
-      total = units * unit_price
-      item_params = {
-        start_date: item_start_date,
-        end_date: item_end_date,
-        unit_price: unit_price,
-        units: units,
-        total: total,
-        invoice: self
-      }
-      LineItem.create(item_params)
+      return [units, unit_price]
     end
-  end
 end
